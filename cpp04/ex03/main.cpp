ac6e2139d94dc6a6e647e93263b0b1643e178f27
IlYAN-FISHERMAN <Ilkay.yanar71@proton.me>
2024-12-20 19:05:15 +0100

    update

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ilyanar <ilyanar>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 16:29:15 by ilyanar           #+#    #+#             */
-/*   Updated: 2024/12/17 18:53:11 by ilyanar          ###   LAUSANNE.ch       */
+/*   Updated: 2024/12/17 20:01:29 by ilyanar          ###   LAUSANNE.ch       */
 /*                                                                            */
 /* ************************************************************************** */
  
@@ -20,7 +20,15 @@ int main(void)
 {
 	Ice givrali("glace");
 	Cure nymphali("fee");
+	Cure nymphali2("fee");
+	Cure nymphali3("fee");
+	Cure nymphali4("fee");
 
+	for (int i = 0; i < 4; i++)
+	{
+		src->learnMateria(&nymphali);
+	}
+	src->learnMateria(&nymphali2);
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(&givrali);
 	src->learnMateria(&nymphali);
